bd44930dccf6405d6107b85066137706ec928fc2
Bryce Schmidt <bryce.schmidt@tstmedia.com>
2014-09-08 17:32:37 -0500

    added binding spec

---
+++ spec/lib/associations/active_record/bindings/many_spec.rb
@@ -0,0 +1,39 @@
+require "spec_helper"
+
+describe ActiveMongoid::Associations::ActiveRecord::Bindings::Many do
+
+  let(:division) do
+    Division.new
+  end
+
+  let(:team) do
+    Team.new
+  end
+
+  let(:division_metadata) do
+    Division.am_relations["teams"]
+  end
+
+  describe "#bind_one" do
+
+    context "when the child of a references many" do
+
+      let(:binder) do
+        described_class.new(division, team, division_metadata)
+      end
+
+      context "when the document is bindable with default" do
+
+        before do
+          binder.bind_one(team)
+        end
+
+        it "sets the inverse relation" do
+          expect(team.division).to eq(division)
+        end
+
+      end
+
+    end
+  end
+end
