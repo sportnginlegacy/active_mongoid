50e42cf75ce57e5c50b07cac53510e99cdabbff1
Bryce Schmidt <bryce.schmidt@tstmedia.com>
2014-09-10 13:09:24 -0500

    moved shared examples under support

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -65,4 +65,3 @@ ActiveRecord::Schema.define do
 end
 
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
-Dir["#{File.dirname(__FILE__)}/shared_examples/**/*.rb"].each { |f| require f }
