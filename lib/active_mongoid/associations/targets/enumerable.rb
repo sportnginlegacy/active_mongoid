3989bd2332fade4820e5b61a34dbb9cec034110b
Bryce Schmidt <bryce.schmidt@tstmedia.com>
2014-09-11 11:15:08 -0500

    overriding enumerable methods to using object_id since AR records do not always have an id

--- lib/active_mongoid/associations/targets/enumerable.rb
+++ lib/active_mongoid/associations/targets/enumerable.rb
@@ -5,6 +5,66 @@ module ActiveMongoid
     module Targets
       class Enumerable < ::Mongoid::Relations::Targets::Enumerable
 
+        def <<(document)
+          id = document.id || document.object_id
+          _added[id] = document
+          self
+        end
+        alias :push :<<
+
+        def delete(document)
+          id = document.id || document.object_id
+          doc = (_loaded.delete(document.id) || _added.delete(id))
+          unless doc
+            if _unloaded && _unloaded.where(_id: document.id).exists?
+              yield(document) if block_given?
+              return document
+            end
+          end
+          yield(doc) if block_given?
+          doc
+        end
+
+        def delete_if(&block)
+          load_all!
+          deleted = in_memory.select(&block)
+          deleted.each do |doc|
+            id = doc.id || doc.object_id
+            _loaded.delete(doc.id)
+            _added.delete(id)
+          end
+          self
+        end
+
+        def each
+          unless block_given?
+            return to_enum
+          end
+          if _loaded?
+            _loaded.each_pair do |id, doc|
+              yield(doc)
+            end
+          else
+            unloaded_documents.each do |doc|
+              id = doc.id || doc.object_id
+              document = _added.delete(doc.id) || _loaded.delete(id) || doc
+              _loaded[document.id] = document
+              yield(document)
+            end
+          end
+          _added.each_pair do |id, doc|
+            yield(doc)
+          end
+          @executed = true
+        end
+
+        def include?(doc)
+          return super unless _unloaded
+          id = doc.id || doc.object_id
+          _unloaded.where(_id: doc.id).exists? || _added.has_key?(doc.id)
+        end
+
+
       end
     end
   end
