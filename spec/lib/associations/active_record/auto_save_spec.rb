a6744dba3b677bb1c4890a9d2c9add3fbc3aa6ed
Bryce Schmidt <bryce.schmidt@tstmedia.com>
2014-09-11 15:55:22 -0500

    autosave functions for has_many_documents

--- spec/lib/associations/active_record/auto_save_spec.rb
+++ spec/lib/associations/active_record/auto_save_spec.rb
@@ -102,6 +102,55 @@ describe ActiveMongoid::Associations::ActiveRecord::AutoSave do
 
     end
 
+    describe "relation is a has_many" do
+
+      before(:all) do
+        Division.autosave_documents(Division.am_relations["teams"].merge!(autosave: true))
+      end
+
+      after(:all) do
+        Division.reset_callbacks(:save)
+      end
+
+      let(:division) do
+        Division.new
+      end
+
+      let(:team) do
+        Team.new(name: "Foo")
+      end
+
+      context "when the option is provided" do
+
+        before do
+          division.teams << team
+        end
+
+        context "when saving the parent document" do
+
+          before do
+            division.save
+          end
+
+          it "does save self" do
+            expect(division).to be_persisted
+          end
+
+          it "does save the relation" do
+            expect(team).to be_persisted
+          end
+
+          it "does save the relation id" do
+            expect(division.id).to_not be_nil
+            expect(team.division_id).to eq(division.id)
+          end
+
+        end
+
+      end
+
+    end
+
   end
 
 end
