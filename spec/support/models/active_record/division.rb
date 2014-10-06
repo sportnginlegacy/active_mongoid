ff36420cb3b2a50bd73342d2a48d35469c303969
Bryce Schmidt <bryce.schmidt@tstmedia.com>
2014-10-06 14:30:54 -0500

    added test for scoped relations

--- spec/support/models/active_record/division.rb
+++ spec/support/models/active_record/division.rb
@@ -7,6 +7,9 @@ class Division < ActiveRecord::Base
   belongs_to_document :league
   belongs_to_document :post, foreign_key: :pid
 
+  scope :sport_id, ->(id){ where(sport_id: id) }
+  scope :by_name, ->(n){ where(name: n) }
+
   bsonify_attr :_id, initialize: true
   bsonify_attr :sport_id
 end
