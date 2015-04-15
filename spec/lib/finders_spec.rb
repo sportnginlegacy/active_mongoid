19a9fb32f8c299a9dfc990e12c7c82f850a3d370
Bryce Schmidt <brycesch@gmail.com>
2015-04-15 14:53:31 -0500

    added tests and support models for allowing ar models to have a bson primary key

--- spec/lib/finders_spec.rb
+++ spec/lib/finders_spec.rb
@@ -1,103 +1,212 @@
 require "spec_helper"
 
 describe ActiveMongoid::Finders do
-  let(:klass) { Division }
-  let(:division) { klass.create(name: 'foo') }
 
   describe ".find" do
 
-    describe "when not scoped" do
+    describe "when primary id is not bson_attr" do
+      let(:klass) { Division }
+      let(:subject) { klass.create(name: 'foo') }
 
-      it "returns object when request with object fixnum id" do
-        expect(klass.find(division.id)).to eq(division)
+      describe "when not scoped" do
+
+        it "returns object when request with object fixnum id" do
+          expect(klass.find(subject.id)).to eq(subject)
+        end
+
+        it "returns object when request with object bson id" do
+          expect(klass.find(subject._id)).to eq(subject)
+        end
+
+        it "raises NotFound exception when request with fixnum id" do
+          expect{klass.find(-1)}.to raise_error(ActiveRecord::RecordNotFound)
+        end
+
+        it "raises NotFound exception when request with bson id" do
+          expect{klass.find(::ActiveMongoid::BSON::ObjectId.new)}.to raise_error(ActiveRecord::RecordNotFound)
+        end
+
       end
 
-      it "returns object when request with object bson id" do
-        expect(klass.find(division._id)).to eq(division)
-      end
+      describe "when scoped" do
 
-      it "raises NotFound exception when request with fixnum id" do
-        expect{klass.find(-1)}.to raise_error(ActiveRecord::RecordNotFound)
-      end
+        let(:scoped_klass) { klass.by_name(subject.name) }
 
-      it "raises NotFound exception when request with bson id" do
-        expect{klass.find(::ActiveMongoid::BSON::ObjectId.new)}.to raise_error(ActiveRecord::RecordNotFound)
+        it "returns object when request with object fixnum id" do
+          expect(scoped_klass.find(subject.id)).to eq(subject)
+        end
+
+        it "returns object when request with object bson id" do
+          expect(scoped_klass.find(subject._id)).to eq(subject)
+        end
+
+        it "raises NotFound exception when request with fixnum id" do
+          expect{scoped_klass.find(-1)}.to raise_error(ActiveRecord::RecordNotFound)
+        end
+
+        it "raises NotFound exception when request with bson id" do
+          expect{scoped_klass.find(::ActiveMongoid::BSON::ObjectId.new)}.to raise_error(ActiveRecord::RecordNotFound)
+        end
+
       end
 
     end
 
-    describe "when scoped" do
+    describe "when primary id is bson_attr" do
+      let(:klass) { Play }
+      let(:subject) { klass.create(name: 'foo') }
 
-      let(:scoped_klass) { klass.by_name(division.name) }
+      describe "when not scoped" do
 
-      it "returns object when request with object fixnum id" do
-        expect(scoped_klass.find(division.id)).to eq(division)
+        it "returns object when request with object fixnum id" do
+          expect(klass.find(subject.id)).to eq(subject)
+        end
+
+        it "returns object when request with object bson id" do
+          expect(klass.find(subject._id)).to eq(subject)
+        end
+
+        it "raises NotFound exception when request with fixnum id" do
+          expect{klass.find(-1)}.to raise_error(ActiveRecord::RecordNotFound)
+        end
+
+        it "raises NotFound exception when request with bson id" do
+          expect{klass.find(::ActiveMongoid::BSON::ObjectId.new)}.to raise_error(ActiveRecord::RecordNotFound)
+        end
+
       end
 
-      it "returns object when request with object bson id" do
-        expect(scoped_klass.find(division._id)).to eq(division)
-      end
+      describe "when scoped" do
 
-      it "raises NotFound exception when request with fixnum id" do
-        expect{scoped_klass.find(-1)}.to raise_error(ActiveRecord::RecordNotFound)
-      end
+        let(:scoped_klass) { klass.by_name(subject.name) }
 
-      it "raises NotFound exception when request with bson id" do
-        expect{scoped_klass.find(::ActiveMongoid::BSON::ObjectId.new)}.to raise_error(ActiveRecord::RecordNotFound)
+        it "returns object when request with object fixnum id" do
+          expect(scoped_klass.find(subject.id)).to eq(subject)
+        end
+
+        it "returns object when request with object bson id" do
+          expect(scoped_klass.find(subject._id)).to eq(subject)
+        end
+
+        it "raises NotFound exception when request with fixnum id" do
+          expect{scoped_klass.find(-1)}.to raise_error(ActiveRecord::RecordNotFound)
+        end
+
+        it "raises NotFound exception when request with bson id" do
+          expect{scoped_klass.find(::ActiveMongoid::BSON::ObjectId.new)}.to raise_error(ActiveRecord::RecordNotFound)
+        end
+
       end
 
     end
 
-  end
+    end
 
   describe ".where" do
 
-    let(:bson_id) { ::ActiveMongoid::BSON::ObjectId.new }
+    describe "when primary_key is not bson_attr" do
+      let(:klass) { Division }
+      let(:subject) { klass.create(name: 'foo') }
+      let(:bson_id) { ::ActiveMongoid::BSON::ObjectId.new }
 
-    before do
-      division.update_attributes(sport_id: bson_id)
+      before do
+        subject.update_attributes(sport_id: bson_id)
+      end
+
+      describe "when not scoped" do
+
+        it "returns relation by bson_id" do
+          expect(klass.where(sport_id: bson_id)).to eq([subject])
+        end
+
+        it "returns relation by bson_id string" do
+          expect(klass.where(sport_id: bson_id.to_s)).to eq([subject])
+        end
+
+        it "return relation by primary key" do
+          expect(klass.where(id: subject.id)).to eq([subject])
+        end
+
+        it "return relation by bson primary key" do
+          expect(klass.where(id: subject._id)).to eq([subject])
+        end
+
+      end
+
+      describe "when scoped" do
+
+        let(:scoped_klass) { klass.by_name(subject.name) }
+
+        it "returns relation by bson_id" do
+          expect(scoped_klass.where(sport_id: bson_id)).to eq([subject])
+        end
+
+        it "returns relation by bson_id string" do
+          expect(scoped_klass.where(sport_id: bson_id.to_s)).to eq([subject])
+        end
+
+        it "return relation by primary key" do
+          expect(scoped_klass.where(id: subject.id)).to eq([subject])
+        end
+
+        it "return relation by bson primary key" do
+          expect(scoped_klass.where(id: subject._id)).to eq([subject])
+        end
+
+      end
+
     end
 
-    describe "when not scoped" do
+    describe "when primary_key is not bson_attr" do
+      let(:klass) { Play }
+      let(:subject) { klass.create(name: 'foo') }
+      let(:bson_id) { ::ActiveMongoid::BSON::ObjectId.new }
 
-      it "returns relation by bson_id" do
-        expect(klass.where(sport_id: bson_id)).to eq([division])
+      before do
+        subject.update_attributes(sport_id: bson_id)
       end
 
-      it "returns relation by bson_id string" do
-        expect(klass.where(sport_id: bson_id.to_s)).to eq([division])
+      describe "when not scoped" do
+
+        it "returns relation by bson_id" do
+          expect(klass.where(sport_id: bson_id)).to eq([subject])
+        end
+
+        it "returns relation by bson_id string" do
+          expect(klass.where(sport_id: bson_id.to_s)).to eq([subject])
+        end
+
+        it "return relation by primary key" do
+          expect(klass.where(id: subject.id)).to eq([subject])
+        end
+
+        it "return relation by bson primary key" do
+          expect(klass.where(id: subject._id)).to eq([subject])
+        end
+
       end
 
-      it "return relation by primary key" do
-        expect(klass.where(id: division.id)).to eq([division])
+      describe "when scoped" do
+
+        let(:scoped_klass) { klass.by_name(subject.name) }
+
+        it "returns relation by bson_id" do
+          expect(scoped_klass.where(sport_id: bson_id)).to eq([subject])
+        end
+
+        it "returns relation by bson_id string" do
+          expect(scoped_klass.where(sport_id: bson_id.to_s)).to eq([subject])
+        end
+
+        it "return relation by primary key" do
+          expect(scoped_klass.where(id: subject.id)).to eq([subject])
+        end
+
+        it "return relation by bson primary key" do
+          expect(scoped_klass.where(id: subject._id)).to eq([subject])
+        end
+
       end
-
-      it "return relation by bson primary key" do
-        expect(klass.where(id: division._id)).to eq([division])
-      end
-
-    end
-
-    describe "when scoped" do
-
-      let(:scoped_klass) { klass.by_name(division.name) }
-
-      it "returns relation by bson_id" do
-        expect(scoped_klass.where(sport_id: bson_id)).to eq([division])
-      end
-
-      it "returns relation by bson_id string" do
-        expect(scoped_klass.where(sport_id: bson_id.to_s)).to eq([division])
-      end
-
-      it "return relation by primary key" do
-        expect(scoped_klass.where(id: division.id)).to eq([division])
-      end
-
-      it "return relation by bson primary key" do
-        expect(scoped_klass.where(id: division._id)).to eq([division])
-      end
-
     end
 
   end
