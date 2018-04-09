6ca67cde550a257170f239a81ffa4fbb1fd0d1d5
Nate Greene <nate.greene@sportngin.com>
2018-04-09 14:35:06 -0500

    use bson

--- lib/active_mongoid.rb
+++ lib/active_mongoid.rb
@@ -4,7 +4,7 @@ require "active_mongoid/finder_proxy"
 require "active_mongoid/finders"
 
 module ActiveMongoid
-  if ::Gem::Version.new(Mongoid::VERSION).between?(::Gem::Version.new("3.0.0"), ::Gem::Version.new("4.0.0"))
+  if ::Gem::Version.new(Mongoid::VERSION).between?(::Gem::Version.new("3.0.0"), ::Gem::Version.new("3.1.7"))
     BSON = ::Moped::BSON
   else
     BSON = ::BSON
